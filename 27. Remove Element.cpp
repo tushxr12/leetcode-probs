48eb5db8de72f463e68710e16ab6a123c9a310ef
tushxr12 <tusharforcourses@gmail.com>
2024-07-29 23:52:56 +0530

    add problem

---
+++ 27. Remove Element.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    int removeElement(vector<int> &nums, int val)
+    {
+        vector<int> temp;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != val)
+            {
+                temp.push_back(nums[i]);
+            }
+        }
+        nums = temp;
+        return temp.size();
+    }
+};
