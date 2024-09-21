2b3dea9182edb3bc89bb45433776ca86589b0585
tushxr12 <tusharforcourses@gmail.com>
2024-09-21 19:34:25 +0530

    Practice tree probs

---
+++ Practice Problems/257. Binary Tree Paths.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+private:
+    void solve(TreeNode *root, vector<int> &currPath, vector<vector<int>> &v)
+    {
+        if (!root)
+            return;
+
+        currPath.push_back(root->val);
+        if (!root->left && !root->right)
+            v.push_back(currPath);
+        else
+        {
+            solve(root->left, currPath, v);
+            solve(root->right, currPath, v);
+        }
+        currPath.pop_back();
+    }
+
+public:
+    vector<string> binaryTreePaths(TreeNode *root)
+    {
+        vector<string> ans;
+        if (!root)
+            return ans;
+        vector<int> currPath;
+        vector<vector<int>> v;
+        solve(root, currPath, v);
+        for (auto i : v)
+        {
+            string curr = "";
+            for (int j = 0; j < i.size(); j++)
+            {
+                if (j == i.size() - 1)
+                {
+                    curr += to_string(i[j]);
+                }
+                else
+                {
+                    curr += (to_string(i[j]) + "->");
+                }
+            }
+            // for(auto j : i)
+            // {
+            //     curr += (to_string(j) + "->");
+            // }
+            ans.push_back(curr);
+        }
+        return ans;
+    }
+};
