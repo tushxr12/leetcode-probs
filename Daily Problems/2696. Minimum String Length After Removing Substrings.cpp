9969851c5f38cab0c98cdc3843e51861f8b1c8f4
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-10-07 10:38:34 +0530

    Practice leetcode daily prob October 7th

---
+++ Daily Problems/2696. Minimum String Length After Removing Substrings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char> st;
+        for(auto i : s)
+        {
+            if(st.empty())
+            {
+                st.push(i);
+            }
+            else if(i == 'B' && st.top() == 'A')
+            {
+                st.pop();
+            }
+            else if(i == 'D' && st.top() == 'C')
+            {
+                st.pop();
+            }
+            else
+            {
+                st.push(i);
+            }
+        }
+        return st.size();
+    }
+};
