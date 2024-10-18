6431742ef16d9f7ea19435d84b0d0cdadd31ca01
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-10-18 10:31:00 +0530

    Practice daily problem october 18th

---
+++ Daily Problems/2044. Count Number of Maximum Bitwise-OR Subsets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int countMaxOrSubsets(vector<int>& nums) {
+        int maxOrValue = 0;
+        for (int num : nums) {
+            maxOrValue |= num;
+        }
+        return countSubsets(nums, 0, 0, maxOrValue);
+    }
+
+private:
+    int countSubsets(vector<int>& nums, int index, int currentOr,
+                     int targetOr) {
+        if (index == nums.size()) {
+            return (currentOr == targetOr) ? 1 : 0;
+        }
+
+        int countWithout = countSubsets(nums, index + 1, currentOr, targetOr);
+
+        int countWith =
+            countSubsets(nums, index + 1, currentOr | nums[index], targetOr);
+
+        return countWithout + countWith;
+    }
+};
