390f61a3950a29abfbf205d0dba7d3d96600df2d
tushxr12 <165460118+tushxr12@users.noreply.github.com>
2024-10-26 16:54:27 +0530

    Practice SQL problem

---
+++ SQL Problems/1045. Customers Who Bought All Products.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT customer_id 
+FROM Customer
+GROUP BY customer_id
+HAVING COUNT(Distinct product_key) = 
+(
+    SELECT COUNT(product_key) from Product
+);
