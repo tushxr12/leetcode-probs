eeb6c1cec83aad3957af05e0847a8656828f88d7
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2024-12-31 12:22:11 +0530

    Add memoization approach

--- Daily Problems/983. Minimum Cost For Tickets.cpp
+++ Daily Problems/983. Minimum Cost For Tickets.cpp
@@ -22,3 +22,34 @@ public:
         return f(days, costs, days[0]);
     }
 };
+
+//Memoization
+class Solution {
+private:
+    unordered_set<int> st;
+    int f(vector<int> &days, vector<int> &costs, int currDay, vector<int> &dp)
+    {
+        if(currDay > days[days.size() - 1])
+            return 0;
+        
+        if(dp[currDay] != -1)
+            return dp[currDay];
+        
+        if(st.find(currDay) == st.end())
+            return dp[currDay] = 0 + f(days, costs, currDay + 1, dp);
+        
+        int oneDay = costs[0] + f(days, costs, currDay + 1, dp);
+        int sevenDays = costs[1] + f(days, costs, currDay + 7, dp);
+        int thirtyDays = costs[2] + f(days, costs, currDay + 30, dp);
+        return dp[currDay] = min({oneDay, sevenDays, thirtyDays});
+    }
+public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        for(auto i : days)
+            st.insert(i);
+
+        int maxi = days[days.size() - 1];
+        vector<int> dp(maxi + 1, -1);
+        return f(days, costs, days[0], dp);
+    }
+};
