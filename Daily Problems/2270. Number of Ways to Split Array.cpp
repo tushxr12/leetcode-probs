b47415c34bdcde9ad267917785bd145528682a93
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-01-03 11:34:47 +0530

    Practice daily leetcode prob Dec 3rd

--- Daily Problems/2270. Number of Ways to Split Array.cpp
+++ Daily Problems/2270. Number of Ways to Split Array.cpp
@@ -17,3 +17,25 @@ public:
         return ans;
     }
 };
+
+//Optimal
+class Solution {
+public:
+    int waysToSplitArray(vector<int>& nums) {
+        int n = nums.size();
+        long long int ans = 0, currSum = 0, totSum = 0, remSum = 0;
+        for(auto i : nums)
+            totSum += i;
+        
+        for(int i = 0;i < n - 1;i++)
+        {
+            currSum += nums[i];
+            remSum = (totSum - currSum);
+            if(currSum >= remSum)
+            {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
