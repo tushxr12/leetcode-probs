3aa1b3020add0c2419aa56f22390ccf5b72a5d31
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-01-06 15:21:09 +0530

    Practice daily problem | Jan 6th

--- Daily Problems/1769. Minimum Number of Operations to Move All Balls to Each Box.cpp
+++ Daily Problems/1769. Minimum Number of Operations to Move All Balls to Each Box.cpp
@@ -18,3 +18,32 @@ public:
         return ans;
     }
 };
+
+//Better
+class Solution {
+public:
+    vector<int> minOperations(string s) {
+        int n = s.size();
+        vector<int> ans(n), left(n), right(n);
+        left[0] = 0;
+        right[n - 1] = 0;
+        int balls = s[0] - '0';
+        for(int i = 1;i < n;i++)
+        {
+            left[i] = left[i - 1] + balls;
+            balls+=(s[i] - '0');
+        }
+        balls = (s[n - 1] - '0');
+        for(int i = n - 2;i >= 0;i--)
+        {
+            right[i] = right[i + 1] + balls;
+            balls+=(s[i] - '0');
+        }
+
+        for(int i = 0;i < n;i++)
+        {
+            ans[i] = (left[i] + right[i]);
+        }
+        return ans;
+    }
+};
