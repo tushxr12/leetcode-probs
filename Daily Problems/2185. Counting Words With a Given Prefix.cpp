8a3e0bb9e149e92ca0f92e1cbd61d1c9a99d7c46
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-01-09 10:32:25 +0530

    practice daily leetcode problem Jan 9th | brute force

---
+++ Daily Problems/2185. Counting Words With a Given Prefix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int ans = 0;
+        int n = words.size();
+        for(int i = 0;i < n;i++)
+        {
+            if(words[i].find(pref) == 0)
+            {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
