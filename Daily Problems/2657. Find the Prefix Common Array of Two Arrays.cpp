01924d04c91a17c1e2b5420a100dadc56b7e7756
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-01-14 12:19:33 +0530

    practice daily leetcode problem Jan 14th

---
+++ Daily Problems/2657. Find the Prefix Common Array of Two Arrays.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        int n = A.size();
+        vector<int> freq(n + 1);
+        vector<int> ans(n);
+        int cnt = 0;
+        for(int i = 0;i < n;i++)
+        {
+            freq[A[i]]++;
+            if(freq[A[i]] == 2)
+                cnt++;
+            
+            freq[B[i]]++;
+            if(freq[B[i]] == 2) 
+                cnt++;
+            
+            ans[i] = cnt;
+        }
+        return ans;
+    }
+};
