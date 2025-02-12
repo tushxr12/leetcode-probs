7436255be8197645e04470cbd3515b1ccf83fdd7
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-02-12 17:30:49 +0530

    practice daily problem feb 12th

--- Daily Problems/2342. Max Sum of a Pair With Equal Sum of Digits.cpp
+++ Daily Problems/2342. Max Sum of a Pair With Equal Sum of Digits.cpp
@@ -28,3 +28,37 @@ public:
         return maxi;
     }
 };
+
+//Better
+class Solution {
+private:
+    long long digitSum(long long number)
+    {
+        long long sum = 0;
+        string s = to_string(number);
+        for(auto i : s)
+            sum += (i - '0');
+        return sum;
+    }
+public:
+    int maximumSum(vector<int>& nums) {
+        int n = nums.size();
+        long long sum = 0, maxi = -1;
+        map<int,int> mpp;
+        for(auto i : nums)
+        {
+            long long digitSumm = digitSum(i);
+            if(mpp.find(digitSumm) != mpp.end())
+            {
+                int prevNum = mpp[digitSumm];
+                maxi = max(maxi, (long long)prevNum + i);
+                mpp[digitSumm] = max(i, prevNum);
+            }
+            else
+            {
+                mpp[digitSumm] = i;
+            }
+        }
+        return maxi;
+    }
+};
