5164555d11c8fbbc96024f399cadfff7076bdafc
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-02-20 13:41:02 +0530

    practice daily leetcode problen

--- Daily Problems/1980. Find Unique Binary String.cpp
+++ Daily Problems/1980. Find Unique Binary String.cpp
@@ -29,3 +29,18 @@ public:
         return curr;
     }
 };
+
+//Better
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        string ans = "";
+        int n = nums.size();
+        for(int i = 0;i < n;i++)
+        {
+            char ch = nums[i][i] == '0' ? '1' : '0';
+            ans += ch;
+        }
+        return ans;
+    }
+};
