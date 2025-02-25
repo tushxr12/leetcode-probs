1044d511ceaeaeb6dfc53beb36d54cef3041066e
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-02-25 13:49:32 +0530

    practice daily leetcode prob Feb 25tj

--- Daily Problems/1524. Number of Sub-arrays With Odd Sum.cpp
+++ Daily Problems/1524. Number of Sub-arrays With Odd Sum.cpp
@@ -17,3 +17,30 @@ public:
         return (ans%mod);
     }
 };
+
+//Linear Time
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr) {
+        const int mod = 1e9 + 7;
+        int n = arr.size();
+        int ec = 1, oc = 0, prefSum = 0;
+        int ans = 0;
+        for(int i = 0;i < n;i++)
+        {
+            prefSum += arr[i];
+            if(prefSum%2)
+            {
+                ans += ec;
+                oc++;
+            }
+            else
+            {
+                ans += oc;
+                ec++;
+            }
+            ans %= mod;
+        }
+        return (ans%mod);
+    }
+};
