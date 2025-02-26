85bc8b8933a21c494f964f047d233813b6dbcc30
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-02-26 14:02:54 +0530

    practice daily problem feb 26th

--- Daily Problems/1749. Maximum Absolute Sum of Any Subarray.cpp
+++ Daily Problems/1749. Maximum Absolute Sum of Any Subarray.cpp
@@ -15,3 +15,28 @@ public:
         return maxSum;
     }
 };
+
+//Better
+class Solution {
+public:
+    int maxAbsoluteSum(vector<int>& nums) {
+        int maxSum = INT_MIN, minSum = INT_MAX;
+        int currPositiveSum = 0, currNegativeSum = 0;
+
+        for(auto i : nums)
+        {
+            //Positive
+            currPositiveSum += i;
+            maxSum = max(maxSum, currPositiveSum);
+            if(currPositiveSum < 0)
+                currPositiveSum = 0;
+            
+            //Negative
+            currNegativeSum += i;
+            minSum = min(minSum, currNegativeSum);
+            if(currNegativeSum > 0)
+                currNegativeSum = 0;
+        }
+        return max(maxSum, abs(minSum));
+    }
+};
