22c388f69f740dcd741b6f7fe15284fddcb6aaad
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-05-13 10:19:19 +0530

    practice daily prob May 13th

---
+++ Daily Problems/3335. Total Characters in String After Transformations I.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    const int M = 1e9 + 7;
+    int lengthAfterTransformations(string s, int t) {
+        vector<int> mp(26, 0);
+
+        for(char &ch : s)
+            mp[ch - 'a']++;
+        
+        for(int count = 1;count <= t;count++)
+        {
+            vector<int> temp(26,0);
+
+            for(int i = 0;i < 26;i++)
+            {
+                char ch = i + 'a';
+                int freq = mp[i];
+
+                if(ch != 'z')
+                {
+                    temp[(ch + 1) - 'a'] = (temp[(ch + 1) - 'a'] + freq)%M;
+                }
+                else
+                {
+                    temp['a'- 'a'] = (temp['a' - 'a'] + freq)%M;
+                    temp['b' - 'a'] = (temp['b'- 'a'] + freq)%M;
+                }
+            }
+            mp = move(temp);
+        }
+
+        int res = 0;
+        for(int i = 0;i < 26;i++)
+        {
+            res = (res + mp[i])%M;
+        }
+        return res;
+    }
+};
