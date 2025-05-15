0e229b8c62557bdb97335a05068632fc4709b7e9
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-05-15 10:34:08 +0530

    practice daily prob May 15th

---
+++ Daily Problems/2900. Longest Unequal Adjacent Groups Subsequence I.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
+        int n = words.size();
+        vector<string> ans;
+        for(int i = 0;i < n;i++)
+        {
+            if(i == 0 || groups[i] != groups[i - 1])
+                ans.push_back(words[i]);
+        }
+        return ans;
+    }
+};
