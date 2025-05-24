2be60c26d441cde478bb9d68e5853452e939e3fd
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-05-24 13:27:18 +0530

    practice daily prob May 24th

---
+++ Daily Problems/2942. Find Words Containing Character.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int> ans;
+        int n = words.size();
+        for(int i = 0;i < n;i++)
+        {
+            auto currWord = words[i];
+            if(currWord.find(x) != string::npos)
+                ans.push_back(i);
+        }
+        return ans;
+    }
+};
