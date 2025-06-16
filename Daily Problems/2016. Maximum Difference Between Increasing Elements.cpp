6799f11e44b62b51fa12e3f4a8c612efa679bd5c
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-06-16 15:57:46 +0530

    practice daily problem june 16th

--- Daily Problems/2016. Maximum Difference Between Increasing Elements.cpp
+++ Daily Problems/2016. Maximum Difference Between Increasing Elements.cpp
@@ -16,3 +16,27 @@ public:
         return (maxi == INT_MIN) ? -1 : maxi;
     }
 };
+
+//Optimal
+//TC->O(N)
+//SC->O(1)
+class Solution {
+public:
+    int maximumDifference(vector<int>& nums) {
+        int n = nums.size();
+        int minElement = nums[0];
+        int maxDiff = -1;
+        for(int j = 1;j < n;j++)
+        {
+            if(nums[j] > minElement)
+            {
+                maxDiff = max(maxDiff, nums[j] - minElement);
+            }
+            else
+            {
+                minElement = nums[j];
+            }
+        }
+        return maxDiff;
+    }
+};
