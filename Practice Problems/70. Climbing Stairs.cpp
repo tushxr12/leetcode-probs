9c8c33866dd1bac38fc84b9e428b119b0b1f2fe1
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-06-24 17:28:24 +0530

    practice lc prob

--- Practice Problems/70. Climbing Stairs.cpp
+++ Practice Problems/70. Climbing Stairs.cpp
@@ -1,4 +1,6 @@
 //Recursion
+//TC->O(2^N)
+//SC->O(N)
 class Solution {
 private:
     int func(int ind)
@@ -13,5 +15,29 @@ private:
 public:
     int climbStairs(int n) {
         return func(n);
+    }
+};
+
+//Memoization
+//TC->O(N)
+//SC->O(N)+O(N)
+class Solution {
+private:
+    int func(int ind, vector<int> &dp)
+    {
+        if(ind <= 1)
+            return 1;
+        
+        if(dp[ind] != -1)
+            return dp[ind];
+        
+        int poss1 = func(ind - 1,dp);
+        int poss2 = func(ind - 2,dp);
+        return dp[ind] = (poss1 + poss2);
+    }
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n + 1,-1);
+        return func(n, dp);
     }
 };
