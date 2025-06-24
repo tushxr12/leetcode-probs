ee960219b8bc6c86790dd8fe5d558bd1e8d59cbe
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-06-24 18:45:20 +0530

    practice dp prob | memoization

--- Practice Problems/Frog jump with K distances.cpp
+++ Practice Problems/Frog jump with K distances.cpp
@@ -27,3 +27,36 @@ public:
         return func(n - 1, heights, k);
     }
 };
+
+//Memoization
+//TC->O(N*K)
+//SC->O(N)
+class Solution {
+private:
+    int func(int ind, vector<int> &heights, int k, vector<int> &dp)
+    {
+        if(ind == 0)
+            return 0;
+
+        if(dp[ind] != -1)
+            return dp[ind];
+        
+        int minSteps = INT_MAX;
+
+        for(int j = 1;j <= k;j++)
+        {
+            if((ind - j) >=0 )
+            {
+                int jump = func(ind - j, heights, k, dp) + abs(heights[ind] - heights[ind - j]);
+                minSteps = min(minSteps, jump);
+            }
+        }
+        return dp[ind] = minSteps;
+    }
+public:
+    int frogJump(vector<int>& heights, int k) {
+        int n = heights.size();
+        vector<int> dp(n, -1);
+        return func(n - 1, heights, k, dp);
+    }
+};
