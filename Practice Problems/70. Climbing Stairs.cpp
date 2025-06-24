25dfd498f2406188e22daea8bdd123b0ecd71b3d
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-06-24 17:30:15 +0530

    practice lc prob

--- Practice Problems/70. Climbing Stairs.cpp
+++ Practice Problems/70. Climbing Stairs.cpp
@@ -41,3 +41,23 @@ public:
         return func(n, dp);
     }
 };
+
+//Tabulation
+//TC->O(N)
+//SC->O(N)
+class Solution {
+public:
+    int climbStairs(int n) {
+
+        if(n <= 1)
+            return 1;
+        
+        vector<int> dp(n + 1,-1);
+        dp[0] = 1, dp[1] = 1;
+        
+        for(int i = 2;i <= n;i++)
+            dp[i] = dp[i - 1] + dp[i - 2];
+
+        return dp[n];
+    }
+};
