151722c26acea6769c04317f9ac2ade201e1dbc9
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-06-25 13:36:28 +0530

    practice dp prob | memoization

--- Practice Problems/Maximum sum of non adjacent elements.cpp
+++ Practice Problems/Maximum sum of non adjacent elements.cpp
@@ -26,3 +26,37 @@ public:
         return func(n - 1, nums);
     }
 };
+
+//Memoization
+//TC->O(N)
+//SC->O(N) + O(N)
+class Solution {
+private:
+    int func(int index, vector<int> &nums, vector<int> &dp)
+    {
+        if(index == 0)
+        {
+            return nums[0];
+        }
+
+        if(index < 0)
+            return 0;
+
+        if(dp[index] != -1)
+            return dp[index];
+
+        //Pick 
+        int sum1 = nums[index] + func(index - 2, nums ,dp);
+
+        //Not Pick
+        int sum2 = func(index - 1, nums, dp);
+        
+        return dp[index] = max(sum1, sum2);
+    }
+public:
+    int nonAdjacent(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        return func(n - 1, nums, dp);
+    }
+};
