67d8e18306df598342b53e75a8c8308bd2965e48
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-06-27 09:57:29 +0530

    practice dp prob

--- Practice Problems/House robber.cpp
+++ Practice Problems/House robber.cpp
@@ -39,3 +39,50 @@ public:
         return max(ans1, ans2);
     }
 };
+
+//Memoization
+//TC->O(2N)
+//SC->O(5N)
+class Solution {
+private:
+    long long int func(int index, vector<int> &nums, vector<int> &dp)
+    {
+        if(index == 0)
+            return nums[0];
+        
+        if(index < 0)
+            return 0;
+        
+        if(dp[index] != -1)
+            return dp[index];
+        
+        int pick = nums[index] + func(index - 2, nums, dp);
+
+        int notPick = 0 + func(index - 1, nums, dp);
+
+        return dp[index] = max(pick, notPick);
+    }
+public:
+    int houseRobber(vector<int>& money) {
+        int n = money.size();
+
+        if(n == 1)
+            return money[0];
+            
+        vector<int> v1,v2;
+        vector<int> dp1(n, -1), dp2(n, -1);
+
+        for(int i = 0;i < n;i++)
+        {
+            if(i != 0)  
+                v1.push_back(money[i]);
+            if(i != n - 1)
+                v2.push_back(money[i]);
+        }
+
+        long long int ans1 = func(n - 2, v1, dp1);
+        long long int ans2 = func(n - 2, v2, dp2);
+
+        return max(ans1, ans2);
+    }
+};
