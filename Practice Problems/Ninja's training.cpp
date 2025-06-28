601333516c99de4641aebe5cd528a45e5d4dac64
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-06-28 11:10:04 +0530

    practice dp prob

--- Practice Problems/Ninja's training.cpp
+++ Practice Problems/Ninja's training.cpp
@@ -73,3 +73,35 @@ public:
         return func(n - 1,3,matrix,dp);
     }
 };
+
+//Tabulation
+//TC->O(N*4*3)
+//SC->O(N*4)
+class Solution {
+public:
+    int ninjaTraining(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        vector<vector<int>> dp(n, vector<int>(4, -1));
+        dp[0][0] = max(matrix[0][1], matrix[0][2]);
+        dp[0][1] = max(matrix[0][0], matrix[0][2]);
+        dp[0][2] = max(matrix[0][0], matrix[0][1]);
+        dp[0][3] = max({matrix[0][0], matrix[0][1], matrix[0][2]});
+
+        for(int day = 1; day < n;day++)
+        {
+            for(int last = 0; last < 4; last++)
+            {
+                int maxi = 0;
+                for(int task = 0; task < 3;task++)
+                {
+                    if(task != last){
+                        int points = matrix[day][task] + dp[day - 1][task];
+                        maxi = max(maxi, points);
+                    }
+                }
+                dp[day][last] = maxi;
+            }
+        }
+        return dp[n - 1][3];
+    }
+};
