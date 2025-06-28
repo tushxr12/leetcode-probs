5515f83e3f75e03bb797fad70a07788c7fff51bb
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-06-28 10:57:37 +0530

    practice dp prob | memoization

--- Practice Problems/Ninja's training.cpp
+++ Practice Problems/Ninja's training.cpp
@@ -32,3 +32,44 @@ public:
         return func(n - 1,3,matrix);
     }
 };
+
+//Memoization
+//TC->O(N*4*3)
+//SC->O(N)+O(N*4)
+class Solution {
+private:
+    int func(int day, int last, vector<vector<int>> &nums, vector<vector<int>> &dp)
+    {
+        if(day == 0)
+        {
+            int maxi = 0;
+            for(int task = 0; task < 3;task++)
+            {
+                if(task != last)
+                    maxi = max(maxi, nums[day][task]);
+            }
+            return maxi;
+        }
+
+        if(dp[day][last] != -1)
+        {
+            return dp[day][last];
+        }
+
+        int maxi = 0;
+        for(int task = 0; task < 3;task++)
+        {
+            if(task != last){
+                int points = nums[day][task] + func(day - 1,task,nums, dp);
+                maxi = max(maxi, points);
+            }
+        }
+        return dp[day][last] = maxi;
+    }
+public:
+    int ninjaTraining(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        vector<vector<int>> dp(n, vector<int>(4, -1));
+        return func(n - 1,3,matrix,dp);
+    }
+};
