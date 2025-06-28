432cf77ebaabdbd0713592a28fa421c7d1bb5462
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-06-28 13:45:25 +0530

    practice dp prob

--- Practice Problems/Grid unique paths.cpp
+++ Practice Problems/Grid unique paths.cpp
@@ -22,3 +22,34 @@ public:
         return func(m - 1,n - 1,m,n);
     }
 };
+
+//Memoization
+//TC->O(M*N)
+//SC->O((N-1)+(M-1))+O(M*N)
+class Solution {
+private:
+    int func(int i, int j, int n, int m, vector<vector<int>> &dp)
+    {
+        if(i == 0 && j == 0)
+            return 1;
+        
+        if(i < 0 || j < 0)
+            return 0;
+        
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        //Top
+        int top = func(i - 1,j,n,m,dp);
+
+        //Left
+        int left = func(i, j - 1,n,m,dp);
+
+        return dp[i][j] = (top + left);
+    }
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return func(m - 1,n - 1,m,n, dp);
+    }
+};
