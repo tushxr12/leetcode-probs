9bb2dcd617085460f33b7f51cf48cf114d6c335e
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-06-29 10:18:28 +0530

    practice dp prob

--- Practice Problems/Minimum Falling Path Sum.cpp
+++ Practice Problems/Minimum Falling Path Sum.cpp
@@ -65,3 +65,47 @@ public:
         return mini;
     }
 };
+
+//Tabulation
+//TC->O(N*M)
+//SC->O(N*M)
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size();
+        int mini = INT_MAX;
+        vector<vector<int>> dp(n, vector<int>(m,-1));
+
+        for(int j = 0;j < m;j++)
+            dp[0][j] = matrix[0][j];
+
+        
+        for(int i = 1;i < n;i++)
+        {
+            for(int j = 0;j < m;j++)
+            {
+                int top = matrix[i][j] + dp[i-1][j];
+
+                int ld = matrix[i][j];
+                if(j - 1 >= 0)
+                    ld += dp[i-1][j-1];
+                else
+                    ld += 1e9;
+                int rd = matrix[i][j];
+                if(j + 1 < m)
+                    rd += dp[i-1][j+1];
+                else
+                    rd += 1e9;
+
+                dp[i][j] = min({top, rd, ld});
+            }
+        }
+
+        for(int j = 0;j < m;j++)
+        {
+            mini = min({mini, dp[n-1][j]});
+        }
+
+        return mini;
+    }
+};
