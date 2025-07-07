0cad3798cf010450da7bc6ff8195f372a990720d
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-07-07 23:31:46 +0530

    practice dp prob

---
+++ Practice Problems/Best time to buy and sell stock II.cpp
@@ -0,0 +1,27 @@
+//Recursion
+class Solution{
+private:
+    int f(int index, int buy, vector<int> &arr,int n)
+    {
+        if(index == n)
+            return 0;
+        
+        long profit = 0;
+
+        if(buy)
+        {
+            profit = max(-arr[index] + f(index + 1,0,arr,n) , f(index + 1,1,arr,n));
+        }
+        else
+        {
+            profit = max(arr[index] + f(index + 1,1,arr,n), f(index+1,0,arr,n));
+        }
+        return profit;
+        
+    }
+public:
+    int stockBuySell(vector<int> arr, int n){
+        return f(0, 1, arr, n);
+    }
+};
+
