cc1e7763b5dba33e38487bee8e56138bf10dde68
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-07-08 10:43:15 +0530

    practice daily prob

--- Daily Problems/1751. Maximum Number of Events That Can Be Attended II.cpp
+++ Daily Problems/1751. Maximum Number of Events That Can Be Attended II.cpp
@@ -27,3 +27,37 @@ public:
         return solve(0,events,n, k);
     }
 };
+
+//Memoization
+class Solution {
+private:
+    int solve(int index, vector<vector<int>> &events, int n, int k, vector<vector<int>> &dp)
+    {
+        if(index >= n || k == 0)
+            return 0;
+        
+        if(dp[index][k] != -1)
+            return dp[index][k];
+        
+        int start = events[index][0];
+        int end = events[index][1];
+        int value = events[index][2];
+
+        int notTake = solve(index + 1, events, n, k, dp);
+
+        int j = index + 1;
+        for(;j < n;j++)
+            if(events[j][0] > end)
+                break;
+        
+        int take = value + solve(j, events, n , k - 1, dp);
+        return dp[index][k] = max(take, notTake);
+    }
+public:
+    int maxValue(vector<vector<int>>& events, int k) {
+        sort(events.begin(), events.end());
+        int n = events.size();
+        vector<vector<int>> dp(n + 1, vector<int>(k + 1,-1));
+        return solve(0,events,n, k, dp);
+    }
+};
