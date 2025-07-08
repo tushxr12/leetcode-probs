6d48f8d12d2d2b3ef989630bf792baa8d152eb01
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-07-08 17:08:50 +0530

    practice dp prob

--- Practice Problems/Best time to buy and sell stock III.cpp
+++ Practice Problems/Best time to buy and sell stock III.cpp
@@ -20,3 +20,32 @@ public:
         return func(0, arr, n, 1, 2);
     }
 };
+
+//Memoization
+class Solution{
+private:
+    int func(int index, vector<int> &arr, int n,int buy, int cap, vector<vector<vector<int>>> &dp)
+    {
+        if(index >= n || cap == 0)
+            return 0;
+        
+        if(dp[index][buy][cap] != -1)
+            return dp[index][buy][cap];
+
+        int profit = 0;
+        if(buy)
+        {
+             profit = max(-arr[index] + func(index + 1,arr,n,0,cap, dp), 0 + func(index + 1,arr,n,1,cap, dp));
+        }
+        else
+        {
+             profit = max(arr[index] + func(index + 1,arr,n,1,cap - 1, dp), func(index + 1, arr, n, 0, cap, dp));
+        }
+        return dp[index][buy][cap] = profit;
+    }
+public:
+    int stockBuySell(vector<int> arr, int n){
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2 ,vector<int>(3, -1)));
+        return func(0, arr, n, 1, 2, dp);
+    }
+};
