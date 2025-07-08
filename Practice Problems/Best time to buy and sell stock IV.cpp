f776edd2c77ad61d120b2fe4552347ceeadf12db
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-07-08 17:12:58 +0530

    practice dp prob

---
+++ Practice Problems/Best time to buy and sell stock IV.cpp
@@ -0,0 +1,23 @@
+//Recursion
+
+class Solution{
+private:
+    int func(int index, vector<int> &arr, int n,int buy, int cap)
+    {
+        if(index >= n || cap == 0)
+            return 0;
+        
+        if(buy)
+        {
+            return max(-arr[index] + func(index + 1,arr,n,0,cap), 0 + func(index + 1,arr,n,1,cap));
+        }
+        else
+        {
+            return max(arr[index] + func(index + 1,arr,n,1,cap - 1), func(index + 1, arr, n, 0, cap));
+        }
+    }
+public:
+    int stockBuySell(vector<int> arr, int n, int k){
+        return func(0,arr,n,1,k);
+    }
+};
