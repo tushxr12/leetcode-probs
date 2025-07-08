10933595b26f9b91cc998d9bbc4372706fcd38ea
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-07-08 17:24:07 +0530

    practice dp prob

--- Practice Problems/Best time to buy and sell stock with transaction fees.cpp
+++ Practice Problems/Best time to buy and sell stock with transaction fees.cpp
@@ -24,3 +24,34 @@ public:
         return f(0, 1, arr, n, fee);
     }
 };
+
+//Memoization
+class Solution{
+private:
+     int f(int index, int buy, vector<int> &arr,int n, int fee, vector<vector<int>> &dp)
+    {
+        if(index == n)
+            return 0;
+
+        if(dp[index][buy] != -1)
+            return dp[index][buy];
+        
+        long profit = 0;
+
+        if(buy)
+        {
+            profit = max(-arr[index] -fee + f(index + 1,0,arr,n,fee, dp) , f(index + 1,1,arr,n,fee, dp));
+        }
+        else
+        {
+            profit = max(arr[index] + f(index + 1,1,arr,n, fee, dp), f(index+1,0,arr,n, fee, dp));
+        }
+        return dp[index][buy] = profit;
+        
+    }
+public:
+    int stockBuySell(vector<int> arr, int n, int fee){
+        vector<vector<int>> dp(n, vector<int>(2,-1));
+        return f(0, 1, arr, n, fee, dp);
+    }
+};
