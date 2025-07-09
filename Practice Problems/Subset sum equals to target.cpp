51045687741afa97f99c11dbbfdadde041b2bb4d
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-07-09 18:17:45 +0530

    practice dp prob

--- Practice Problems/Subset sum equals to target.cpp
+++ Practice Problems/Subset sum equals to target.cpp
@@ -30,3 +30,37 @@ public:
         return func(n - 1,0,arr,target);
     }
 };
+
+//Memoization
+class Solution{   
+private:
+    bool func(int index, vector<int> &arr, int target, vector<vector<int>> &dp)
+    {
+        if(target == 0)
+            return true;
+        
+        if(index == 0)
+            return target == arr[index];
+        
+        if(dp[index][target] != -1)
+            return dp[index][target];
+        
+        //Not take 
+        bool notTake = func(index - 1, arr, target, dp);
+
+        //Take 
+        bool take = false;
+        if(arr[index] <= target)
+        {
+            take = func(index - 1, arr, target - arr[index], dp);
+        }
+
+        return dp[index][target] = (take || notTake);
+    }
+public:
+    bool isSubsetSum(vector<int>arr, int target){
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(target + 1,-1));
+        return func(n - 1,arr,target, dp);
+    }
+};
