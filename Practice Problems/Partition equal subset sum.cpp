cfdd4e97782a28f244796bc5c009cbeb2aa9b75b
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-07-10 14:29:11 +0530

    practice dp prob

--- Practice Problems/Partition equal subset sum.cpp
+++ Practice Problems/Partition equal subset sum.cpp
@@ -30,3 +30,41 @@ public:
         return func(n - 1,arr,n,sum/2);
     }
 };
+
+//Memoization
+class Solution{
+private:
+    bool func(int index, vector<int> &arr, int n, int target, vector<vector<int>> &dp)
+    {
+        if(target == 0)
+            return true;
+        
+        if(index == 0)
+            return (target == arr[0]);
+
+        if(dp[index][target] != -1)
+            return dp[index][target];
+        
+        //Not pick
+        bool notPick = func(index - 1, arr, n, target,dp);
+
+        //Pick 
+        bool pick = false;
+        if(arr[index] <= target)
+            pick = func(index - 1, arr, n, target - arr[index],dp);
+
+        return dp[index][target] = (pick || notPick);
+    }
+public:
+    bool equalPartition(int n, vector<int> arr) {
+        int sum = 0;
+        for(auto i : arr)
+            sum += i;
+        
+        if(sum%2)
+            return false;
+        
+        vector<vector<int>> dp(n, vector<int>(sum + 1,-1));
+        return func(n - 1,arr,n,sum/2,dp);
+    }
+};
