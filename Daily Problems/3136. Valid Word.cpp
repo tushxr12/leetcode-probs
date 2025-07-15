aa6295a5f8762f1de15febfeeee6930b5fe95f99
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-07-15 14:29:04 +0530

    practice daily prob July 15th

---
+++ Daily Problems/3136. Valid Word.cpp
@@ -0,0 +1,58 @@
+class Solution {
+private:
+    bool isVowel(char c)
+    {
+        set<char> st = {'a', 'e' ,'i' ,'o' ,'u', 'A' , 'E', 'I', 'O', 'U'};
+        return (st.find(c) != st.end());
+    }
+
+    bool isConsonant(char c)
+    {
+        if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+        {
+            if(isVowel(c))
+                return false;
+            return true;
+        }
+        return false;
+    }
+
+public:
+    bool isValid(string word) {
+        int n = word.size();
+        if(n < 3)
+        {
+            return false;
+        }
+
+        bool vf = false, cf = false;
+
+        for(int i = 0;i < n;i++)
+        {
+            cout<<"Curr char : "<<word[i]<<endl;
+            if((isVowel(word[i])))
+            {
+                cout<<"Inside vowel : ";
+                vf = true;
+            }   
+            else if(isConsonant(word[i]))
+            {
+                cout<<"Inside consonant : ";
+                cf = true;
+            }   
+            else if(isdigit(word[i])){
+                cout<<"Inside digit : ";
+            }
+            else
+            {
+                cout<<"Inside special char : ";
+                return false;
+            }
+                
+        }
+
+        if(vf && cf)
+            return true;
+        return false;
+    }
+};
