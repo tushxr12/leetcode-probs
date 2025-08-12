168253b57d222ecde366c5a8202a7f58d837f4b6
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-08-12 13:17:42 +0530

    practice daily prob Aug 22

--- Daily Problems/2787. Ways to Express an Integer as Sum of Powers.cpp
+++ Daily Problems/2787. Ways to Express an Integer as Sum of Powers.cpp
@@ -22,3 +22,33 @@ public:
         return solve(n, 1, x);
     }
 };
+
+// Using DP
+class Solution {
+private:
+    int solve(int n,int num,int x, vector<vector<int>> &dp){
+        if(n == 0)
+            return 1;
+        
+        if(n < 0)
+            return 0;
+        
+        int currPowerValue = pow(num, x);
+        if(currPowerValue > n)
+            return 0;
+
+        if(dp[n][num] != -1)
+            return dp[n][num];
+
+        int take = solve(n - currPowerValue, num + 1,x, dp);
+        int skip = solve(n, num + 1, x, dp);
+
+        return dp[n][num] = (take + skip)%mod;
+    }
+public:
+    int mod = 1e9 + 7;
+    int numberOfWays(int n, int x) {
+        vector<vector<int>> dp(301, vector<int>(301, -1));
+        return solve(n, 1, x, dp);
+    }
+};
