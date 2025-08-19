6c61ea4b96087aeb192fa5ab0b057c1ae55b2ab4
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-08-19 12:20:47 +0530

    practice daily prob

--- Daily Problems/2348. Number of Zero-Filled Subarrays.cpp
+++ Daily Problems/2348. Number of Zero-Filled Subarrays.cpp
@@ -25,3 +25,23 @@ public:
         return ans;
     }
 };
+
+//Approach-2
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        int n = nums.size();
+        long long ans = 0;
+        int cnt = 0;
+
+        for(int i = 0;i < n;i++){
+            if(nums[i] == 0)
+                cnt++;
+            else
+                cnt = 0;
+            
+            ans += (cnt);
+        }
+        return ans;
+    }
+};
