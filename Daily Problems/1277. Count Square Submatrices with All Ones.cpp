b02e214363a3c154bffe135057dbbdc179815397
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-08-20 10:54:16 +0530

    practice daily prob

--- Daily Problems/1277. Count Square Submatrices with All Ones.cpp
+++ Daily Problems/1277. Count Square Submatrices with All Ones.cpp
@@ -28,3 +28,39 @@ public:
         return res;
     }
 };
+
+//Memoization
+class Solution {
+private:
+    int solve(int i, int j,int n,int m, vector<vector<int>> &matrix, vector<vector<int>> &dp){
+        if(i >= n || j >= m)
+            return 0;
+        
+        if(matrix[i][j] == 0)
+            return 0;
+
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        int right = solve(i,j+1,n,m,matrix,dp);
+        int diag = solve(i+1,j+1,n,m,matrix,dp);
+        int bottom = solve(i+1,j,n,m,matrix,dp);
+
+        return dp[i][j] = 1 + min({right, diag, bottom});
+    }
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1,-1));
+
+        int res = 0;
+        for(int i = 0;i < n;i++){
+            for(int j = 0;j < m;j++){
+                if(matrix[i][j] == 1)
+                    res += solve(i,j,n,m,matrix,dp);
+            }
+        }
+        return res;
+    }
+};
