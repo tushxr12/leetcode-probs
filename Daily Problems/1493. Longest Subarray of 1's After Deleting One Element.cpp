f2585e138ba0676ea9b6bea9a95e9641e2bd6a6d
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-08-24 12:46:11 +0530

    practice daily problem

--- Daily Problems/1493. Longest Subarray of 1's After Deleting One Element.cpp
+++ Daily Problems/1493. Longest Subarray of 1's After Deleting One Element.cpp
@@ -37,3 +37,37 @@ public:
         return cntZero == 0 ? n - 1 : res;
     }
 };
+
+// Better
+// TC -> O(N)
+// SC -> O(1)
+
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0, j = 0;
+        int maxLength = 0;
+        int zeroCount = 0;
+
+        while(j < n){
+            if(nums[j] == 0){
+                zeroCount++;
+            }
+
+            while(zeroCount > 1){
+                if(nums[i] == 0)
+                    zeroCount--;
+                i++;
+            }
+
+            int currLength = j - i;
+            maxLength = max(maxLength, currLength);
+            j++;
+        }
+        return maxLength;
+    }
+};
+
+
+
