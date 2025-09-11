dd43bdaaf98cc0a7c03210c2c0021087337de64b
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-09-11 09:54:43 +0530

    practice daily prob Sept 11

--- Daily Problems/2785. Sort Vowels in a String.cpp
+++ Daily Problems/2785. Sort Vowels in a String.cpp
@@ -34,3 +34,49 @@ public:
         return ans;
     }
 };
+
+
+// Without using external sort [Better]
+class Solution {
+private:
+    bool isVowel(char c){
+        set<char> st = {'a', 'A', 'e' , 'E', 'i', 'I' ,'o', 'O', 'u', 'U'};
+        return (st.find(c) != st.end());
+    }
+public:
+    string sortVowels(string s) {
+        string ans = "";
+        // vector<char> vowels;
+        unordered_map<char, int> mpp;
+        for(auto i: s){
+            if(isVowel(i))
+                mpp[i]++;
+        }
+
+        // sort(begin(vowels), end(vowels));
+
+        string vowels = "AEIOUaeiou";
+        // cout<<"Sorted vowels : ";
+        // for(auto i : vowels)
+        //     cout<<i<<" ";
+        // cout<<endl;
+
+        int index = 0;
+
+        for(auto i : s){
+            if(!isVowel(i))
+            {
+                ans.push_back(i);
+            }
+            else
+            {
+                while(mpp[vowels[index]] == 0)
+                    index++;
+                
+                ans.push_back(vowels[index]);
+                mpp[vowels[index]]--;
+            }
+        }
+        return ans;
+    }
+};
