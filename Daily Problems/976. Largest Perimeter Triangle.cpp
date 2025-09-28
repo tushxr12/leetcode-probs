4e555d87e1cc09526cb26d7fdee8645be62b6f10
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-09-28 11:30:47 +0530

    practice daily prob Sept 28

---
+++ Daily Problems/976. Largest Perimeter Triangle.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        int n = nums.size();
+        sort(begin(nums), end(nums));
+
+        int maxPer = 0;
+
+        for(int i = n - 3;i >= 0;i--){
+            if(nums[i] + nums[i + 1] > nums[i + 2]){
+                return nums[i] + nums[i + 1] + nums[i + 2];
+            }
+        }
+
+        return maxPer;
+    }
+};
