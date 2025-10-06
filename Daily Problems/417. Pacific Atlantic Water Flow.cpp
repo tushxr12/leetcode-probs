119ff4fb8336a4ec00f5e6edacc8eb4c731031eb
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-10-06 10:19:13 +0530

    practice daily prob Oct 6th

---
+++ Daily Problems/417. Pacific Atlantic Water Flow.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<vector<int>> dirs = {{1,0}, {-1,0}, {0,-1} , {0,1}};
+
+    void DFS(vector<vector<int>> &heights, int i,int j, int prevCell, vector<vector<bool>>& visited)
+    {
+        if(i<0 || i >= heights.size() || j < 0 || j >= heights[0].size())
+            return;
+        
+        if(heights[i][j] < prevCell || visited[i][j])
+            return;
+
+        visited[i][j] = true;
+        for(auto &dir: dirs){
+            int i_ = i + dir[0];
+            int j_ = j + dir[1];
+
+            DFS(heights,i_,j_,heights[i][j],visited);
+        }
+    }
+
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        int m = heights.size(), n = heights[0].size();
+
+        vector<vector<int>> ans;
+        vector<vector<bool>> pacificVisited(m, vector<bool>(n, false));
+        vector<vector<bool>> atlanticVisited(m, vector<bool>(n, false));
+
+
+        // Top row and last row
+        for(int j = 0;j < n;j++){
+            DFS(heights,0,j,INT_MIN,pacificVisited);
+            DFS(heights,m-1,j,INT_MIN,atlanticVisited);
+        }
+
+        // First Col and Last Col
+        for(int i = 0;i < m;i++){
+            DFS(heights,i,0,INT_MIN,pacificVisited);
+            DFS(heights,i,n-1,INT_MIN,atlanticVisited);
+        }
+
+        for(int i = 0;i < m;i++){
+            for(int j = 0;j < n;j++){
+                if(pacificVisited[i][j] && atlanticVisited[i][j])
+                    ans.push_back({i,j});
+            }
+        }
+
+
+        return ans;
+    }
+};
