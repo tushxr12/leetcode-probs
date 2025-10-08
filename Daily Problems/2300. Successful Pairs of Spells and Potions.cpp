922148494cd486cea64672c10a0ea843158faae0
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-10-08 10:59:45 +0530

    practice daily prob oct 8th

--- Daily Problems/2300. Successful Pairs of Spells and Potions.cpp
+++ Daily Problems/2300. Successful Pairs of Spells and Potions.cpp
@@ -16,3 +16,28 @@ public:
         return res;
     }
 };
+
+// Optimal
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        vector<int> res;
+        sort(begin(potions), end(potions));
+        int n = potions.size();
+        int maxPotion = potions[n - 1];
+        for(auto i : spells){
+            long long currSpell = i;
+            long long ans = 0;
+            long long minPotion = ceil((success*1.0)/currSpell);
+            if(minPotion > maxPotion){
+                res.push_back(0);
+                continue;
+            }
+            auto it = lower_bound(begin(potions), end(potions),minPotion) - begin(potions);
+            cout<<"Lower bound : it = : "<<it<<endl;
+            ans += potions.size() - it;
+            res.push_back(ans);
+        }
+        return res;
+    }
+};
