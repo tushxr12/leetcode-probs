fc0ad8e41fa28a64ece09923e14d051dde476723
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-10-10 23:08:25 +0530

    practice daily prob oct 10th

--- Daily Problems/3147. Taking Maximum Energy From the Mystic Dungeon.cpp
+++ Daily Problems/3147. Taking Maximum Energy From the Mystic Dungeon.cpp
@@ -22,3 +22,33 @@ public:
         return maxEnergy;
     }
 };
+
+// Memoization
+class Solution {
+public:
+    int n,K;
+
+    int solve(vector<int> &energy, int i,vector<int> &dp)
+    {
+        if(i >= n){
+            return 0;
+        }
+
+        if(dp[i] != INT_MIN)
+            return dp[i];
+
+        return dp[i] = energy[i] + solve(energy, i + K,dp);
+    }
+
+    int maximumEnergy(vector<int>& energy, int k) {
+        n = energy.size();
+        K = k;
+
+        vector<int> dp(n, INT_MIN);
+
+        int maxEnergy = INT_MIN;
+        for(int i = 0;i < n;i++)
+            maxEnergy = max(maxEnergy, solve(energy,i,dp));
+        return maxEnergy;
+    }
+};
