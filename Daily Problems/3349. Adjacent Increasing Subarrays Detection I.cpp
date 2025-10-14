0d31cb86323732f87e11e3c20dacd36d5aea0ecc
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-10-14 23:37:15 +0530

    practice daily prob oct 14th

--- Daily Problems/3349. Adjacent Increasing Subarrays Detection I.cpp
+++ Daily Problems/3349. Adjacent Increasing Subarrays Detection I.cpp
@@ -20,3 +20,32 @@ public:
         return false;
     }
 };
+
+// Better or Cleaner
+class Solution {
+public:
+    bool hasIncreasingSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        int currRun = 1, prevRun = 0, maxRes = 0;
+
+        for(int i = 1;i < n;i++){
+            if(nums[i] > nums[i - 1])
+            {
+                currRun++;
+            }
+            else
+            {
+                prevRun = currRun;
+                currRun = 1;
+            }
+
+            if(currRun/2 >= k)
+                return true;
+            
+            if(min(currRun, prevRun) >= k)
+                return true;
+        }
+        return false;
+    }
+};
