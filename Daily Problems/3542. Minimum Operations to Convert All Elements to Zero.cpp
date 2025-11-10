9aedead5d3237b5c80f929b027dd384f1a9dc8a5
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-11-10 23:24:31 +0530

    practice daily prob nov 10th

--- Daily Problems/3542. Minimum Operations to Convert All Elements to Zero.cpp
+++ Daily Problems/3542. Minimum Operations to Convert All Elements to Zero.cpp
@@ -27,3 +27,28 @@ public:
         return ans;
     }
 };
+
+// Using monotonic stack:
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int n = nums.size();
+        stack<int> st;
+
+        int ans = 0;
+        for(int i = 0;i < n;i++){
+            while(!st.empty() && st.top() > nums[i])
+                st.pop();
+            
+            if(nums[i] == 0)
+                continue;
+            
+            if(st.empty() || st.top() < nums[i])
+            {
+                st.push(nums[i]);
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
