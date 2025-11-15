4bd7095bff53c400e9d1327d36b86b3318e11875
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-11-15 14:59:43 +0530

    practice daily prob nov 15th

--- Daily Problems/3234. Count the Number of Substrings With Dominant Ones.cpp
+++ Daily Problems/3234. Count the Number of Substrings With Dominant Ones.cpp
@@ -33,3 +33,51 @@ public:
         return ans;
     }
 };
+
+// Better
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int n = s.size();
+
+        vector<int> cumOnes(n, 0);
+        cumOnes[0] = (s[0] == '1') ? 1 : 0;
+
+        for(int i = 1;i < n;i++)
+            cumOnes[i] = (cumOnes[i-1]) + (s[i] == '1' ? 1 : 0);
+
+        int ans = 0;
+        for(int i = 0;i < n;i++){
+            for(int j = i;j < n;j++){
+                int oneCount = cumOnes[j] - (i - 1 >= 0 ? cumOnes[i - 1] : 0);
+                int zeroCount = (j - i + 1) - oneCount;
+
+                if((zeroCount*zeroCount) > oneCount){
+                    int waste = (zeroCount*zeroCount)-oneCount;
+                    j += waste - 1;
+                }
+                else if((zeroCount*zeroCount) == oneCount){
+                    ans += 1;
+                }
+                else
+                {
+                    ans += 1;
+
+                    int k = sqrt(oneCount) - zeroCount;
+                    int next = j + k;
+
+                    if(next >= n){
+                        ans += (n - j - 1);
+                        break;
+                    }
+                    else
+                    {
+                        ans += k;
+                    }
+                    j = next;
+                }
+            }
+        }
+        return ans;
+    }
+};
