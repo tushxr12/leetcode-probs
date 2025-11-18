60d1ceacec63dfaa15f0ef098995de87fdc06a00
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-11-18 23:13:57 +0530

    practice daily prob nov 18th

--- Daily Problems/717. 1-bit and 2-bit Characters.cpp
+++ Daily Problems/717. 1-bit and 2-bit Characters.cpp
@@ -12,3 +12,19 @@ public:
         return (i == n - 1);
     }
 };
+
+// Approach - 2
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        int n = bits.size();
+        
+        int cnt = 0;
+
+        for(int i = n - 2; i >= 0 && bits[i] == 1;i--){
+            cnt++;
+        }
+
+        return (cnt%2 == 0) ? true : false;
+    }
+};
