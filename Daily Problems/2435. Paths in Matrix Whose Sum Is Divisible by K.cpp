2b5ceaeb36e245b2d2da9844fd9bdb115a31854f
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-11-26 17:38:44 +0530

    practice daily prob nov 26th

--- Daily Problems/2435. Paths in Matrix Whose Sum Is Divisible by K.cpp
+++ Daily Problems/2435. Paths in Matrix Whose Sum Is Divisible by K.cpp
@@ -23,3 +23,33 @@ public:
         return solve(0,0,0,k,n,m,grid);
     }
 };
+
+// Memoization
+class Solution {
+private:
+    int mod = 1e9 + 7;
+
+    int solve(int row, int col, int currSum, int k, int n,int m, vector<vector<int>>& grid, vector<vector<vector<int>>> &dp)
+    {
+        if(row >= n || col >= m)
+            return 0;
+
+        if(row == n - 1 && col == m - 1){
+            return (currSum + grid[row][col])%k == 0;
+        }
+
+        if(dp[row][col][currSum] != -1)
+            return dp[row][col][currSum];
+
+        int down = solve(row+1,col,(currSum + grid[row][col])%k,k,n,m,grid, dp);
+        int right = solve(row,col + 1,(currSum+grid[row][col])%k,k,n,m,grid, dp);
+
+        return dp[row][col][currSum] = (down + right)%mod;
+    }
+public:
+    int numberOfPaths(vector<vector<int>>& grid, int k) {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(k,-1)));
+        return solve(0,0,0,k,n,m,grid,dp);
+    }
+};
