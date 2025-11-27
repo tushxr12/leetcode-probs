0bb46acb7f4e2c093f2b9df831993b3a8a91d232
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-11-27 17:20:52 +0530

    practice daily prob nov 27th
    Implemented an optimal solution using Kadane's algorithm and prefix sums to find the maximum subarray sum with a length divisible by k.

--- Daily Problems/3381. Maximum Subarray Sum With Length Divisible by K.cpp
+++ Daily Problems/3381. Maximum Subarray Sum With Length Divisible by K.cpp
@@ -20,3 +20,38 @@ public:
         return ans;
     }
 };
+
+
+// Optimal (Kadanes + Prefix Sum)
+class Solution {
+public:
+    long long maxSubarraySum(vector<int>& nums, int k) {
+        long long ans = LLONG_MIN;
+        int n = nums.size();
+
+        vector<long long> prefSum(n);
+        prefSum[0] = nums[0];
+
+        for(int i = 1;i < n;i++)
+            prefSum[i] = prefSum[i-1] + nums[i];
+        
+        for(int start = 0; start < k;start++){
+            long long currSum = 0;
+
+            int i = start;
+
+            while(i < n && i + k - 1 < n){
+                int j = i + k - 1;
+
+                long long subarraySum = prefSum[j] - ((i > 0) ? prefSum[i - 1] : 0);
+                currSum = max(subarraySum, currSum + subarraySum);
+                ans = max(ans, currSum);
+
+                i += k;
+            }
+        }
+
+
+        return ans;
+    }
+};
