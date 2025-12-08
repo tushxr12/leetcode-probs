4217128184a21d460fcb9ac1dc037813f2ac0351
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-12-09 00:07:13 +0530

    practice daily prob dec 9th

--- Daily Problems/1925. Count Square Sum Triples.cpp
+++ Daily Problems/1925. Count Square Sum Triples.cpp
@@ -14,3 +14,22 @@ public:
         return ans;
     }
 };
+
+// Better
+class Solution {
+public:
+    int countTriples(int n) {
+        int ans = 0;
+        for(int i = 1;i <= n;i++){
+            for(int j = i+1;j <= n;j++){
+                int sum = i*i + j*j;
+                int x = sqrt(sum);
+
+                if(x*x == sum && x <= n){
+                    ans += 2;
+                }
+            }
+        }
+        return ans;
+    }
+};
