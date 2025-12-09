74b40dd9b388b76d3864a4ded99265c7c85ef64a
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-12-09 23:46:05 +0530

    practice daily prob dec 9

--- Daily Problems/3583. Count Special Triplets.cpp
+++ Daily Problems/3583. Count Special Triplets.cpp
@@ -18,3 +18,31 @@ public:
         return ans;
     }
 };
+
+// Better (2 pass)
+class Solution {
+public:
+    const int mod = 1e9 + 7;
+    int specialTriplets(vector<int>& nums) {
+        int n = nums.size();
+        long long ans = 0;
+        unordered_map<int,int> leftMap, rightMap;
+
+        for(int i = 0;i < n;i++){
+            rightMap[nums[i]]++;
+        }
+
+        for(int i = 0;i < n;i++){
+            rightMap[nums[i]]--;
+
+            int left = leftMap[2*nums[i]];
+            int right = rightMap[2*nums[i]];
+
+            ans = (ans + (1LL *left*right)) % mod;
+
+            leftMap[nums[i]]++;
+        }
+
+        return ans;
+    }
+};
