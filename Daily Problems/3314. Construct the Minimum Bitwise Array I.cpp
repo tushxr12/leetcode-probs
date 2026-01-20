5f4d23f5c84f59f1a635f2257423ce716b6622f5
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-01-20 23:22:56 +0530

    practice daily prob jan 20th

---
+++ Daily Problems/3314. Construct the Minimum Bitwise Array I.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vector<int> ans;
+        int n = nums.size();
+
+        for(int i = 0;i < n;i++)
+        {
+            if(nums[i] == 2)
+            {
+                ans.push_back(-1);
+                continue;
+            }
+            
+            bool found = false;
+            for(int j = 1;j < 32;j++)
+            {
+                if((nums[i] & (1 << j)) > 0) // jth bit is set
+                {
+                    continue;
+                }
+
+                int prev = j - 1;
+                // set it to 0
+                int x = (nums[i] ^ (1 << (j-1)));
+                ans.push_back(x);
+                found = true;
+                break;
+            }
+            if(!found)
+                ans.push_back(-1);
+        }
+        return ans;
+    }
+};
