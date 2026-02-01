b1497bf092cf918e409ba9134c5ad097f67fb0f4
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-02-01 13:03:14 +0530

    practice daily prob

--- Daily Problems/3010. Divide an Array Into Subarrays With Minimum Cost I.cpp
+++ Daily Problems/3010. Divide an Array Into Subarrays With Minimum Cost I.cpp
@@ -10,3 +10,28 @@ public:
         return ans;
     }
 };
+
+// Better
+class Solution {
+public:
+    int minimumCost(vector<int>& nums) {
+        int n = nums.size();
+        int ans = nums[0];
+        int firstMin = INT_MAX, secondMin = INT_MAX;
+
+        for(int i = 1;i < n;i++)
+        {
+            if(nums[i] < firstMin)
+            {
+                secondMin = firstMin;
+                firstMin = nums[i];
+            }
+            else if(nums[i] < secondMin)
+            {
+                secondMin = nums[i];
+            }
+        }
+
+        return (ans + firstMin + secondMin);
+    }
+};
