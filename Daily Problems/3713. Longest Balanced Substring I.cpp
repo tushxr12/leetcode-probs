c8c5f83207afe7fe1586f9b22c1c58170824979d
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-02-12 23:58:11 +0530

    practice daily prob feb 12th

---
+++ Daily Problems/3713. Longest Balanced Substring I.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool checkBalanced(vector<int> &freq)
+    {
+        int common = 0;
+        for(int i = 0;i < 26;i++){
+            if(freq[i] == 0)
+                continue;
+            if(common == 0)
+                common = freq[i];
+            else if(freq[i] != common)
+                return false;
+        }
+        return true;
+    }
+
+    int longestBalanced(string s) {
+        int n = s.size();
+        int maxi = 1;
+
+        for(int i = 0;i < n;i++)
+        {
+            vector<int> freq(26, 0);
+            for(int j = i;j < n;j++)
+            {
+                freq[s[j] - 'a']++;
+                if(checkBalanced(freq)){
+                    maxi = max(maxi, j-i+1);
+                }
+            }
+        }
+
+
+        return maxi;
+    }
+};
