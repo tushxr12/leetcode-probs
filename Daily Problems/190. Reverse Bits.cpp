5f224a8619a3264939a9a3ff7c4b22f7e203b1d5
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-02-16 23:22:16 +0530

    practice daily prob feb 16th

---
+++ Daily Problems/190. Reverse Bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        if(n == 0)
+            return 0;
+        
+        int res = 0;
+        for(int i = 1;i <= 32;i++){
+            res <<= 1;
+            res = (res | ((n&1)));
+            n >>= 1;
+        }
+
+        return res;
+    }
+};
