d5697af2bc7b588ff039dd55a2c78334050ed81e
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-02-22 17:35:38 +0530

    practice Dali prob Feb 22

---
+++ Daily Problems/868. Binary Gap.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        int prev = -1;
+        int res = 0;
+
+        for(int curr = 0; curr < 32;curr++){
+            if(((n >> curr) & 1) > 0){
+                res = (prev != -1) ? max(res, curr - prev) : res;
+                prev = curr;
+            }
+        }
+        
+        return res;
+    }
+};
