c4f05f2be793be556147f814876143c55b71ac5a
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-02-26 23:08:34 +0530

    practice daily prob feb 26

---
+++ Daily Problems/1404. Number of Steps to Reduce a Number in Binary Representation to One.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numSteps(string s) {
+        int n = s.size();
+        int op = 0;
+        int carry = 0;
+
+        for(int i = n - 1;i >= 1;i--){
+            if(((s[i] - '0') + carry)%2 == 1)
+            {
+                op += 2;
+                carry = 1;
+            }
+            else
+            {
+                op += 1;
+            }
+        }
+        return (op + carry);
+    }
+};
