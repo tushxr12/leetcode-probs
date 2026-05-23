c26552b9bb5203e568b3022297b99a6df0906ae5
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-05-23 23:17:30 +0530

    practice daily prob may 23rd

---
+++ Daily Problems/1752. Check if Array Is Sorted and Rotated_May23.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+
+        if(n <= 1)
+            return true;
+
+        for(int i = 1;i < n;i++)
+        {
+            if(nums[i-1] > nums[i])
+                count++;
+        }
+
+        if(nums[0] < nums[n-1]) 
+            count++;
+        return (count <= 1);
+    }
+};
