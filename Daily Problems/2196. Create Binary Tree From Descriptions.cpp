ec017033faf0b0e4c11a16115e18f1a5291b7ae4
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-06-07 14:01:02 +0530

    practice daily prob jun2 7

---
+++ Daily Problems/2196. Create Binary Tree From Descriptions.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        unordered_map<int, TreeNode*> mpp;
+        unordered_set<int> childSet;
+
+        for(vector<int> &description : descriptions)
+        {
+            int parent = description[0];
+            int child = description[1];
+            int isLeft = description[2];
+
+            if(mpp.find(parent) == mpp.end())
+            {
+                mpp[parent] = new TreeNode(parent);
+            }
+
+            if(mpp.find(child) == mpp.end())
+            {
+                mpp[child] = new TreeNode(child);
+            }
+
+            if(isLeft)
+            {
+                mpp[parent]->left = mpp[child];
+            }
+            else
+            {
+                mpp[parent]->right = mpp[child];
+            }
+
+            childSet.insert(child);
+        }
+
+        for(auto &description : descriptions)
+        {
+            int parent = description[0];
+            if(childSet.find(parent) == childSet.end())
+                return mpp[parent];
+        }
+        return NULL;
+    }
+};
