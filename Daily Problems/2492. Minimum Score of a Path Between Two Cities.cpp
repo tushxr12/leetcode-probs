f6b43441e731b33b6950990daa5d90fe150e1962
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-07-04 14:03:56 +0530

    practice daily prob July 4th

--- Daily Problems/2492. Minimum Score of a Path Between Two Cities.cpp
+++ Daily Problems/2492. Minimum Score of a Path Between Two Cities.cpp
@@ -1,3 +1,4 @@
+// Using DFS
 class Solution {
 private:
     void dfs(int node, vector<bool> &visited, unordered_map<int, vector<pair<int,int>>> &adj, int& res)
@@ -33,6 +34,59 @@ public:
         vector<bool> visited(n, false);
         int res = INT_MAX;
         dfs(1,visited,adj,res);
+        return res;
+    }
+};
+
+// Using BFS
+class Solution {
+private:
+    void bfs(int node, unordered_map<int, vector<pair<int,int>>> &adj, vector<bool> &visited, int &res)
+    {
+        queue<int> q;
+        q.push(node);
+        visited[node] = true;
+
+        while(!q.empty())
+        {
+            auto i = q.front();
+            q.pop();
+
+            for(auto it : adj[i])
+            {
+                int v = it.first;
+                int d = it.second;
+
+                res = min(res, d);
+
+                if(!visited[v])
+                {
+                    visited[v] = true;
+                    q.push(v);
+                }
+            }
+
+        }
+    }
+public:
+    int minScore(int n, vector<vector<int>>& roads) {
+        vector<bool> visited(n + 1, false);
+        unordered_map<int, vector<pair<int,int>>> adj;
+
+        for(auto &vec : roads)
+        {
+            int u = vec[0];
+            int v = vec[1];
+            int d = vec[2];
+
+            adj[u].push_back({v, d});
+            adj[v].push_back({u, d});
+        }
+
+        int res = INT_MAX;
+        
+        bfs(1, adj, visited, res);
+        
         return res;
     }
 };
