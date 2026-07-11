29e885ba176812fb50be4cd6284c378e62ba2a41
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-07-11 13:46:42 +0530

    solve daily prob july 11th

--- Daily Problems/2685. Count the Number of Complete Components.cpp
+++ Daily Problems/2685. Count the Number of Complete Components.cpp
@@ -48,3 +48,70 @@ public:
         return ans;
     }
 };
+
+// Using BFS
+class Solution {
+public:
+    void bfs(int node, unordered_map<int,vector<int>> &adj, vector<bool> &visited, int &v, int &e)
+    {
+        queue<int> q;
+        q.push(node);
+        visited[node] = true;
+
+        while(!q.empty())
+        {
+            int sz = q.size();
+
+            for(int i = 0;i < sz;i++)
+            {
+                auto node = q.front();
+                q.pop();
+
+                v++;
+                e += adj[node].size();
+
+                for(auto &ngbr : adj[node])
+                {
+                    if(!visited[ngbr])
+                    {
+                        visited[ngbr] = true;
+                        q.push(ngbr);
+                    }
+                }
+
+            }
+        }
+
+    }
+
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        unordered_map<int, vector<int>> adj;
+        int ans = 0;
+
+        for(auto &edge : edges)
+        {
+            int u = edge[0];
+            int v = edge[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<bool> visited(n,false);
+
+        for(int i = 0;i < n;i++)
+        {
+            if(!visited[i])
+            {
+                int v = 0, e = 0;
+                bfs(i, adj, visited, v, e);
+                if((v*(v-1) == e))
+                {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
