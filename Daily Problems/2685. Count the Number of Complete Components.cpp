1b56884afe690ff4e3892467e427a9fc68ebbb9d
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-07-11 13:10:44 +0530

    solve daily prob july 11th

---
+++ Daily Problems/2685. Count the Number of Complete Components.cpp
@@ -0,0 +1,50 @@
+// Using DFS
+class Solution {
+public:
+    void dfs(int node, unordered_map<int,vector<int>> &adj, vector<bool> &visited, int &v, int &e)
+    {
+        visited[node] = true;
+        v++;
+        e += adj[node].size();
+
+        for(auto &ngbr : adj[node])
+        {
+            if(!visited[ngbr])
+            {
+                dfs(ngbr, adj, visited,v,e);
+            }
+        }
+
+    }
+
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        unordered_map<int, vector<int>> adj;
+        int ans = 0;
+
+        for(auto &edge : edges)
+        {
+            int u = edge[0];
+            int v = edge[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<bool> visited(n,false);
+
+        for(int i = 0;i < n;i++)
+        {
+            if(!visited[i])
+            {
+                int v = 0, e = 0;
+                dfs(i, adj, visited, v, e);
+                if((v*(v-1) == e))
+                {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
