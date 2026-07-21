5074e64fb3078b6be4311c49066d135161d94ff1
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-07-21 19:49:07 +0530

    practice daily prob july 22nd

---
+++ Daily Problems/3499. Maximize Active Section with Trade I.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maxActiveSectionsAfterTrade(string s) {
+        int n = s.size();
+        int onesCount = count(begin(s), end(s), '1');
+
+        vector<int> inactiveBlocks;
+        int i = 0;
+
+        while(i < n)
+        {
+            if(s[i] == '0')
+            {
+                int start = i;
+                while(i < n && s[i] == '0')
+                    i++;
+                inactiveBlocks.push_back(i-start);
+            }
+            else
+            {
+                i++;
+            }
+        }
+
+        int maxPairSum = 0;
+        for(int i = 1;i < inactiveBlocks.size();i++)
+        {
+            maxPairSum = max(maxPairSum, inactiveBlocks[i-1] + inactiveBlocks[i]);
+        }
+        return maxPairSum + onesCount;
+    }
+};
